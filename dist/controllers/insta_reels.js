cb98574be278f5ccaa680b387735560f556f27c6
Ken <quytd@mcrew-tech.com>
2023-09-13 19:59:32 +0700

    feat: add instagram reels

--- dist/controllers/insta_reels.js
+++ dist/controllers/insta_reels.js
@@ -23,33 +23,26 @@ function instaReels(req, res, next) {
             .split("/")
             .filter((x) => x.length > 0)[3];
         try {
-            const resp = yield (0, axios_1.default)(domain);
-            //REQUEST CONFIG
-            var config = {
-                method: "get",
-                url: `${domain}${postId}/?utm_source=ig_web_copy_link?&__a=1&__d=1`,
+            const resp = yield axios_1.default.get(`${domain}${postId}`, {
                 headers: {
                     "sec-fetch-dest": "document",
                     "sec-fetch-mode": "navigate",
                     "sec-fetch-site": "none",
                     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52",
                     'Cookie': process.env.INSTAGRAM_COOKIE
-                }
-            };
-            //REQUEST
-            (0, axios_1.default)(config)
-                .then(function (response) {
-                const urls = response.data.items[0].video_versions;
-                const thumbs = response.data.items[0].image_versions2.candidates;
-                const title = response.data.items[0].caption.text;
-                res.send({ thumbs, urls, title });
-            })
-                .catch(function (error) {
-                res.send({ error1: error.message });
+                },
+                params: {
+                    __a: 1,
+                    __d: 1
+                },
             });
+            const urls = resp.data.items[0].video_versions;
+            const thumbs = resp.data.items[0].image_versions2.candidates;
+            const title = resp.data.items[0].caption.text;
+            res.send({ thumbs, urls, title });
         }
         catch (error) {
-            res.send({ error2: error.message });
+            res.send({ error: error.message });
         }
     });
 }
--- src/controllers/insta_reels.ts
+++ src/controllers/insta_reels.ts
@@ -9,33 +9,25 @@ export async function instaReels(req, res, next) {
         .filter((x: String) => x.length > 0)[3];
 
     try {
-        const resp = await axios(domain);
-
-        //REQUEST CONFIG
-        var config = {
-            method: "get",
-            url: `${domain}${postId}/?utm_source=ig_web_copy_link?&__a=1&__d=1`,
+        const resp = await axios.get(`${domain}${postId}`, {
             headers: {
                 "sec-fetch-dest": "document",
                 "sec-fetch-mode": "navigate",
                 "sec-fetch-site": "none",
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52",
                 'Cookie': process.env.INSTAGRAM_COOKIE
-            }
-        };
+            },
+            params: {
+                __a: 1,
+                __d: 1
+            },
+        });
 
-        //REQUEST
-        axios(config)
-            .then(function (response) {
-                const urls = response.data.items[0].video_versions;
-                const thumbs = response.data.items[0].image_versions2.candidates;
-                const title = response.data.items[0].caption.text;
-                res.send({ thumbs, urls, title });
-            })
-            .catch(function (error) {
-                res.send({ error1: error.message });
-            });
+        const urls = resp.data.items[0].video_versions;
+        const thumbs = resp.data.items[0].image_versions2.candidates;
+        const title = resp.data.items[0].caption.text;
+        res.send({ thumbs, urls, title });
     } catch (error) {
-        res.send({ error2: error.message });
+        res.send({ error: error.message });
     }
 }
